fe08705402a1af3d9cd4435d5e274e23ab15cb5d
cupcakes-code <jbryant1015@gmail.com>
2020-12-16 07:19:44 -0500

    adding my react app tp github pages

---
+++ src/components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    ' Hi, Im jessica an aspiring software developer. I have an Associates in social science and currently getting my BS in Software development. I have many intereste and hobbies including drawing, painting, coding, and learning karate and kick boxing.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'This site was created using React and gatsby I enjoyed this project. Some of my other projects and designs are in the works I am always creating new projects and ideas. I know Html, JavaScript, Css(also tailwind css), React, Gatsby, Nodejs and much more. '}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'I currently have a Microsoft Fundamentals, Project +, and ITIL fundamentals certification and more to come. My contact info is at the bottom of the page always open for new projects and oppertunities.'}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,86 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: ' Jessica Bryant', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: ' Welcome thanks for stopping by', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi, Im',//  
+  name: 'Jessica D Bryant', //
+  subtitle: 'Aspiring Front end/ Ux developer',// Aspiring Front end/ Ux developer
+  cta: 'Thanks for stopping By.',// 
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profilepic.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume:  '<embed type="application/pdf" src="C:\Users\Jbrya\OneDrive\Desktop\reactportfolio\my-gatsby-project\mate-portfolio\gatsby-simplefolio\src\components\Jessica Bryant Resume.docx" style= width=100% height 600px> </embed>', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cupcakes-code/C-project-array.git', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '202-560-1656',
+  btn: 'Email me',
+  email: 'Jbryant1015@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    
+    
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/jessica-bryant-709036173/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/cupcakes-code',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
